97285038a084d0f68ecdb28959c5d5a9a9c449c8
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2021-07-25 20:25:59 -0400

    adding drawer layout to  xml.

--- app/src/main/java/com/example/fragment_eplantion/Main1Activity.java
+++ app/src/main/java/com/example/fragment_eplantion/Main1Activity.java
@@ -1,14 +1,38 @@
 package com.example.fragment_eplantion;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class Main1Activity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawer,R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(actionBarDrawerToggle );
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
