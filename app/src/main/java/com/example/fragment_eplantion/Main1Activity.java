e86c7cef1519fe78d706d27ba0c4bb21f602dcfe
shadabsheikh51024225 <shadabsheikh07@gmail.com>
2021-07-25 20:59:14 -0400

    adding drawer layout to  xml.

--- app/src/main/java/com/example/fragment_eplantion/Main1Activity.java
+++ app/src/main/java/com/example/fragment_eplantion/Main1Activity.java
@@ -1,6 +1,7 @@
 package com.example.fragment_eplantion;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,8 +9,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Main1Activity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Main1Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
     @Override
@@ -19,11 +24,42 @@ public class Main1Activity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         drawer = findViewById(R.id.drawer_layout);
-
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawer,R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
         drawer.addDrawerListener(actionBarDrawerToggle );
         actionBarDrawerToggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Chat_fragment()).commit();
+            navigationView.setCheckedItem(R.id.chat);
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Chat_fragment()).commit();
+                break;
+            case R.id.email:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Email_fragment()).commit();
+                break;
+            case R.id.user:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new User_fragment()).commit();
+                break;
+            case R.id.share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.send:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
@@ -35,4 +71,6 @@ public class Main1Activity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
 }
